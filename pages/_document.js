6101566c5842f010f55e7fa380960e184ae3fef1
Ryuichi Paul E. Egoshi <it.engr.ryuichi@gmail.com>
2021-03-08 21:32:22 +0900

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-842SVY7MNS" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-842SVY7MNS');
+            `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,6 @@ export default class Home extends Component {
             <title>ゲストハウス愛徳 | 公式ホームページ</title>
             <link rel="icon" href="https://gaitoku.herokuapp.com/logolove.jpg" />
             <meta charSet="utf-8" />
-            <meta name="google-site-verification" content="ym6_1XKOzOZ7W4q6QZ6n4GKT3iTM-S0yPcaF2v5zc0w" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="title" content="ゲストハウス愛徳 | 公式ホームページ" />
             <meta name="description" content="「ただいま」って言いたくなるあたたかい宿泊施設です。野沢温泉村、ゲストハウス愛徳で宿泊しませんか？ ゲレンデまで徒歩1分。スキーシーズンもグリーンシーズンも楽しもう！スキー、スノーボードは勿論、夏は涼しく自然の中でリモートワークも最適！" />
